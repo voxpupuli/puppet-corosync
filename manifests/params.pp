d017600bc827da2611f5f5438753f7e2c3adf20d
Jurgen Weber <j.weber@foodpanda.com>
2015-02-14 09:10:25 +0000

    setup votequorum for ubuntu 14.04 lts, needed it configured so to get a quorum

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,23 @@
+#
+#
+#
 class corosync::params {
-  $enable_secauth    = true
-  $authkey_source    = 'file'
-  $authkey           = '/etc/puppet/ssl/certs/ca.pem'
-  $threads           = $::processorcount
-  $port              = '5405'
-  $bind_address      = $::ipaddress
-  $multicast_address = 'UNSET'
-  $unicast_addresses = 'UNSET'
-  $force_online      = false
-  $check_standby     = false
-  $debug             = false
-  $rrp_mode          = 'none'
-  $ttl               = false
-  $packages          = ['corosync', 'pacemaker']
-  $token             = 3000
+  $enable_secauth     = true
+  $authkey_source     = 'file'
+  $authkey            = '/etc/puppet/ssl/certs/ca.pem'
+  $threads            = $::processorcount
+  $port               = '5405'
+  $bind_address       = $::ipaddress
+  $multicast_address  = 'UNSET'
+  $unicast_addresses  = 'UNSET'
+  $force_online       = false
+  $check_standby      = false
+  $debug              = false
+  $rrp_mode           = 'none'
+  $ttl                = false
+  $packages           = ['corosync', 'pacemaker']
+  $token              = 3000
+  $set_expected_votes = '2'
 
   case $::osfamily {
     'RedHat': {
