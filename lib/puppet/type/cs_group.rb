bd4268e760b70d58c2286e8d378805c5a13f0cdd
Florian Haas <florian@hastexo.com>
2015-07-15 01:07:39 +0200

    Allow 1-element arrays for primitives in cs_group
    Having only one primitive in a group is a perfectly valid use
    case. For example, one might have constraints related to an arbitrary
    number of primitives created with create_resources, and the simplest
    way of doing that is to place them all of them in a group, and have
    the constraint point to that. Disallowing the possibility of ending up
    with just one primitive in the group does not make much sense.
    
    Update the check to allow 1-element arrays. Also, remove a misleading
    comment that also looks like it was copied from some other code that
    talked about colocations, not groups.
    
    Fixes issue puppet-community/puppet-corosync#152.

--- lib/puppet/type/cs_group.rb
+++ lib/puppet/type/cs_group.rb
@@ -20,14 +20,20 @@ Puppet::Type.newtype(:cs_group) do
     desc "An array of primitives to have in this group.  Must be listed in the
       order that you wish them to start."
 
+    # We want this to be an array, even if it has only one
+    # value. Prior to 4.x (and unless using the future parser in
+    # 3.x), Puppet would munge single-parameter arrays into scalar
+    # values. See also
+    # https://tickets.puppetlabs.com/browse/PUP-1299.
+    munge do |value|
+      Array(value)
+    end
+
     # Have to redefine should= here so we can sort the array that is given to
     # us by the manifest.  While were checking on the class of our value we
-    # are going to go ahead and do some validation too.  The way Corosync
-    # colocation works we need to only accept two value arrays.
-    def should=(value)
-      super
-      raise Puppet::Error, 'Puppet::Type::Cs_Group: primitives property must be at least a 2-element array.' unless value.is_a?(Array) && value.length > 1
-      @should
+    # are going to go ahead and do some validation too.
+    validate do |value|
+      raise Puppet::Error, "Puppet::Type::Cs_Group: primitives property must be at least a 1-element array." unless value.is_a? Array and value.length >= 1
     end
   end
 
