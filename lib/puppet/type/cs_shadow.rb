635b13390ddb006e0d0e2b2e7b41ea5d2a72d9f7
Julien Pivotto <roidelapluie@inuits.eu>
2015-09-08 11:27:36 +0200

    Use Puppet::Type.newtype instead of Puppet.newtype
    In Puppet 4 we see the following warning:
    
        Warning: Creating cs_property via Puppet.newtype is deprecated and will
        be removed in a future release. Use Puppet::Type.newtype instead.
           (at /opt/puppetlabs/puppet/lib/ruby/vendor_ruby/puppet.rb:145:in
        `newtype')
    
    This commit removes it.
    
    It is compatible with older versions of Puppet.

--- lib/puppet/type/cs_shadow.rb
+++ lib/puppet/type/cs_shadow.rb
@@ -1,38 +1,36 @@
-module Puppet
-  newtype(:cs_shadow) do
-    @doc = "cs_shadow resources represent a Corosync shadow CIB. Any corosync
-      resources defined with 'cib' set to the title of a cs_shadow resource
-      will not become active until all other resources with the same cib
-      value have also been applied."
+Puppet::Type.newtype(:cs_shadow) do
+  @doc = "cs_shadow resources represent a Corosync shadow CIB. Any corosync
+    resources defined with 'cib' set to the title of a cs_shadow resource
+    will not become active until all other resources with the same cib
+    value have also been applied."
 
-    newproperty(:cib) do
-      def sync
-        provider.sync(self.should)
-      end
-
-      def retrieve
-        :absent
-      end
-
-      def insync?(is)
-        false
-      end
-
-      defaultto { @resource[:name] }
+  newproperty(:cib) do
+    def sync
+      provider.sync(self.should)
     end
 
-    newparam(:name) do
-      desc "Name of the shadow CIB to create and manage"
-      isnamevar
+    def retrieve
+      :absent
     end
 
-    def generate
-      options = { :name => @title }
-      [ Puppet::Type.type(:cs_commit).new(options) ]
+    def insync?(is)
+      false
     end
 
-    autorequire(:service) do
-      [ 'corosync' ]
-    end
+    defaultto { @resource[:name] }
+  end
+
+  newparam(:name) do
+    desc "Name of the shadow CIB to create and manage"
+    isnamevar
+  end
+
+  def generate
+    options = { :name => @title }
+    [ Puppet::Type.type(:cs_commit).new(options) ]
+  end
+
+  autorequire(:service) do
+    [ 'corosync' ]
   end
 end
