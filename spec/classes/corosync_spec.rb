voxpupuli/puppet-corosync

commit 9a54a5c6b7974eba7456bdcd3bd6c654ed15350c

Do not set two_nodes on three-node cluster with expected_votes = 2
Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

templates/corosync.conf.erb
@@ -78,16 +78,10 @@ aisexec {
 quorum {
   provider: corosync_votequorum
 <% if @votequorum_expected_votes -%>
-<% if @votequorum_expected_votes.to_i > 2 -%>
   expected_votes: <%= @votequorum_expected_votes %>
-<% else -%>
-  expected_votes: <%= @votequorum_expected_votes %>
-  two_node: 1
 <% end -%>
-<% else -%>
 <% if [@quorum_members].flatten.count == 2 -%>
   two_node: 1
-<% end -%>
 <% end -%>
 }
 

spec/classes/corosync_spec.rb
@@ -27,6 +27,9 @@ describe 'corosync' do
         should contain_file('/etc/corosync/corosync.conf').with_content(
           %r{ring0_addr\: node2\.test\.org\n\s*nodeid: 2}
         )
+        should contain_file('/etc/corosync/corosync.conf').with_content(
+          %r{two_node: 1}
+        )
       end
 
       it 'supports persistent node IDs' do
@@ -39,6 +42,22 @@ describe 'corosync' do
         )
         should contain_file('/etc/corosync/corosync.conf').with_content(
           %r{ring0_addr\: node2\.test\.org\n\s*nodeid: 11}
+        )
+      end
+    end
+
+    context 'when set_quorum is true and quorum_members are set and there are 3 nodes' do
+      before :each do
+        params.merge!(
+          set_votequorum: true,
+          quorum_members: ['node1.test.org', 'node2.test.org', 'node3.test.org'],
+          votequorum_expected_votes: 2
+        )
+      end
+
+      it 'does not configure two_nodes option' do
+        should_not contain_file('/etc/corosync/corosync.conf').with_content(
+          %r{two_node: 1}
         )
       end
     end

templates/corosync.conf.udpu.erb
@@ -79,16 +79,10 @@ aisexec {
 quorum {
   provider: corosync_votequorum
 <% if @votequorum_expected_votes -%>
-<% if @votequorum_expected_votes.to_i > 2 -%>
   expected_votes: <%= @votequorum_expected_votes %>
-<% else -%>
-  expected_votes: <%= @votequorum_expected_votes %>
-  two_node: 1
 <% end -%>
-<% else -%>
 <% if [@quorum_members].flatten.count == 2 -%>
   two_node: 1
-<% end -%>
 <% end -%>
 }
 

commit 6cc6e678e8b9f95741a460c666e033ddf87cb477

Fix two_nodes behaviour with expected_votes = 2
commit 9a54a5c6b7974eba7456bdcd3bd6c654ed15350c introduced a wrong
behaviour: when expected_votes is 2, then two_nodes should be set to 1,
even  if you do not specify quorum_members

Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

templates/corosync.conf.erb
@@ -100,8 +100,10 @@ quorum {
   provider: corosync_votequorum
 <% if @votequorum_expected_votes -%>
   expected_votes: <%= @votequorum_expected_votes %>
+<% if @votequorum_expected_votes == 2 -%>
+  two_node: 1
 <% end -%>
-<% if [@quorum_members].flatten.count == 2 -%>
+<% elsif [@quorum_members].flatten.count == 2 -%>
   two_node: 1
 <% end -%>
 }

spec/classes/corosync_spec.rb
@@ -51,11 +51,26 @@ describe 'corosync' do
         params.merge!(
           set_votequorum: true,
           quorum_members: ['node1.test.org', 'node2.test.org', 'node3.test.org'],
-          votequorum_expected_votes: 2
+          votequorum_expected_votes: 3
         )
       end
 
       it 'does not configure two_nodes option' do
+        should_not contain_file('/etc/corosync/corosync.conf').with_content(
+          %r{two_node: 1}
+        )
+      end
+    end
+
+    context 'when set_quorum is true and votequorum_expected_votes is 2' do
+      before do
+        params.merge!(
+          set_votequorum: true,
+          votequorum_expected_votes: 2
+        )
+      end
+
+      it 'does configure two_nodes option' do
         should_not contain_file('/etc/corosync/corosync.conf').with_content(
           %r{two_node: 1}
         )
@@ -268,7 +283,7 @@ describe 'corosync' do
           )
         end
 
-        it 'is set in corosync.conf' do
+        it 'is present in corosync.conf' do
           should contain_file('/etc/corosync/corosync.conf').with_content(
             %r{#{optional_parameter}:\s*#{possible_value}\n}
           )