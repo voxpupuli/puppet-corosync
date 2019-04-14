640366635b463fbfdbd83bd12977f378c566f827
Tobias Wolter <tobias.wolter@b1-systems.de>
2019-04-14 20:11:25 +0200

    Reduce delay for systemd stop of pacemaker.service
    The sane-y default of a 30 minute timeout is too much when travis kills
    hanging executions after ten minutes.
    
    The real cause for the restart to hang is not very clear yet (seems to
    be related to stonith being enabled while not having anything
    configured), but this at least makes the tests finish.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -44,3 +44,33 @@ def cleanup_cs_resources
   apply_manifest(pp, catch_failures: true, debug: false, trace: true)
   apply_manifest(pp, catch_changes: true, debug: false, trace: true)
 end
+
+RSpec.shared_context 'with faked facts' do
+  let(:facts_d) do
+    puppet_version = get_puppet_version
+    if fact('osfamily') =~ %r{windows}i
+      if fact('kernelmajversion').to_f < 6.0
+        'C:/Documents and Settings/All Users/Application Data/PuppetLabs/facter/facts.d'
+      else
+        'C:/ProgramData/PuppetLabs/facter/facts.d'
+      end
+    elsif Puppet::Util::Package.versioncmp(puppet_version, '4.0.0') < 0 && fact('is_pe', '--puppet') == 'true'
+      '/etc/puppetlabs/facter/facts.d'
+    else
+      '/etc/facter/facts.d'
+    end
+  end
+
+  before do
+    # No need to create on windows, PE creates by default
+    shell("mkdir -p '#{facts_d}'") if fact('osfamily') !~ %r{windows}i
+  end
+
+  after do
+    shell("rm -f '#{facts_d}/fqdn.txt'", acceptable_exit_codes: [0, 1])
+  end
+
+  def fake_fact(name, value)
+    shell("echo #{name}=#{value} > '#{facts_d}/#{name}.txt'")
+  end
+end
