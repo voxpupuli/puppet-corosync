7af4a0eb363889d36cb4843e6b094570c6af7905
Tim Eilers <tim.eilers@kps.com>
2019-05-08 10:19:16 +0200

    Fixing rubocop issues
    Still trying to fix #447

--- lib/facter/pacemakerd_version.rb
+++ lib/facter/pacemakerd_version.rb
@@ -1,6 +1,5 @@
 Facter.add(:pacemakerd_version) do
   setcode do
-
     pacemakerd_version_string = Facter::Util::Resolution.exec('pacemakerd --version 2>&1')
 
     unless pacemakerd_version_string.nil?
