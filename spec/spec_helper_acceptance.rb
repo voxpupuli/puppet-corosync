14a31416f3c4bb321f1d65575d01f8edf0bb2fa0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2019-10-19 15:22:11 +0200

    Clean up acceptance spec helper

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,17 +1,9 @@
-# This file comes from puppetlabs-stdlib
-# which is licensed under the Apache-2.0 License.
-# https://github.com/puppetlabs/puppetlabs-stdlib
-# (c) 2015-2015 Puppetlabs and puppetlabs-stdlib contributors
-
 require 'beaker-rspec'
 require 'beaker-puppet'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 
 run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
   # Readable test descriptions
@@ -19,10 +11,10 @@ RSpec.configure do |c|
 
   # Configure all nodes in nodeset
   c.before :suite do
+    install_module
+    install_module_dependencies
+
     hosts.each do |host|
-      if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
-        on(host, "sed -i '/nodocs/d' /etc/yum.conf")
-      end
       # For Debian 8 "jessie", we need
       # - pacemaker and crmsh delivered in jessie-backports only
       # - openhpid post-install may fail (https://bugs.debian.org/785287)
