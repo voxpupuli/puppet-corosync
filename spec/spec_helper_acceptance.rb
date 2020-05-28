6d3042b8d53ab876f50d96a8af262b2a0c7c0bf9
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-05-28 15:49:48 +0200

    Use voxpupuli-acceptance

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,43 +1,19 @@
-# This file comes from puppetlabs-stdlib
-# which is licensed under the Apache-2.0 License.
-# https://github.com/puppetlabs/puppetlabs-stdlib
-# (c) 2015-2015 Puppetlabs and puppetlabs-stdlib contributors
+require 'voxpupuli/acceptance/spec_helper_acceptance'
 
-require 'beaker-rspec'
-require 'beaker-puppet'
-require 'beaker/puppet_install_helper'
-require 'beaker/module_install_helper'
-
-run_puppet_install_helper unless ENV['BEAKER_provision'] == 'no'
-install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
-install_module_on(hosts)
-install_module_dependencies_on(hosts)
-
-RSpec.configure do |c|
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    hosts.each do |host|
-      if host[:platform] =~ %r{el-7-x86_64} && host[:hypervisor] =~ %r{docker}
-        on(host, "sed -i '/nodocs/d' /etc/yum.conf")
-      end
-      # For Debian 8 "jessie", we need
-      # - pacemaker and crmsh delivered in jessie-backports only
-      # - openhpid post-install may fail (https://bugs.debian.org/785287)
-      if fact('os.family') == 'Debian' && fact('os.release.major') == '8'
-        on host, 'echo deb http://ftp.debian.org/debian jessie-backports main >> /etc/apt/sources.list'
-        on host, 'apt-get update && apt-get install -y openhpid', acceptable_exit_codes: [0, 1, 100]
-      end
-      # On Debian 9 "stretch", service state transitions (restart, stop) hang indefinitely and
-      # lead to test timeouts if there is a service unit of Type=notify involved.
-      # Use Type=simple as a workaround. See issue 455.
-      if host[:hypervisor] =~ %r{docker} && fact('os.family') == 'Debian' && fact('os.release.major') == '9'
-        on host, 'mkdir /etc/systemd/system/corosync.service.d'
-        on host, 'echo -e "[Service]\nType=simple" > /etc/systemd/system/corosync.service.d/10-type-simple.conf'
-      end
-    end
+configure_beaker do |host|
+  # For Debian 8 "jessie", we need
+  # - pacemaker and crmsh delivered in jessie-backports only
+  # - openhpid post-install may fail (https://bugs.debian.org/785287)
+  if fact_on(host, 'os.family') == 'Debian' && fact_on(host, 'os.release.major') == '8'
+    on host, 'echo deb http://ftp.debian.org/debian jessie-backports main >> /etc/apt/sources.list'
+    on host, 'apt-get update && apt-get install -y openhpid', acceptable_exit_codes: [0, 1, 100]
+  end
+  # On Debian 9 "stretch", service state transitions (restart, stop) hang indefinitely and
+  # lead to test timeouts if there is a service unit of Type=notify involved.
+  # Use Type=simple as a workaround. See issue 455.
+  if host[:hypervisor] =~ %r{docker} && fact_on(host, 'os.family') == 'Debian' && fact_on(host, 'os.release.major') == '9'
+    on host, 'mkdir /etc/systemd/system/corosync.service.d'
+    on host, 'echo -e "[Service]\nType=simple" > /etc/systemd/system/corosync.service.d/10-type-simple.conf'
   end
 end
 
@@ -68,34 +44,4 @@ def cleanup_cs_resources
 
   apply_manifest(pp, catch_failures: true, debug: false, trace: true)
   apply_manifest(pp, catch_changes: true, debug: false, trace: true)
-end
-
-RSpec.shared_context 'with faked facts' do
-  let(:facts_d) do
-    puppet_version = get_puppet_version
-    if fact('osfamily') =~ %r{windows}i
-      if fact('kernelmajversion').to_f < 6.0
-        'C:/Documents and Settings/All Users/Application Data/PuppetLabs/facter/facts.d'
-      else
-        'C:/ProgramData/PuppetLabs/facter/facts.d'
-      end
-    elsif Puppet::Util::Package.versioncmp(puppet_version, '4.0.0') < 0 && fact('is_pe', '--puppet') == 'true'
-      '/etc/puppetlabs/facter/facts.d'
-    else
-      '/etc/facter/facts.d'
-    end
-  end
-
-  before do
-    # No need to create on windows, PE creates by default
-    shell("mkdir -p '#{facts_d}'") if fact('osfamily') !~ %r{windows}i
-  end
-
-  after do
-    shell("rm -f '#{facts_d}/fqdn.txt'", acceptable_exit_codes: [0, 1])
-  end
-
-  def fake_fact(name, value)
-    shell("echo #{name}=#{value} > '#{facts_d}/#{name}.txt'")
-  end
 end
