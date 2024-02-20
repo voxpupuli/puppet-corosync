189dd44ec7fb7f63a18af2c3193f0ad49aedfea2
markuszilch <zilchmarkus@web.de>
2024-02-20 14:00:33 +0100

    revert to previous greater than syntax

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -18,10 +18,10 @@ configure_beaker do |host|
       pcs_version = '0.10.0'
     when 'Ubuntu'
       default_provider = 'pcs'
-      pcs_version = if facts[:os]['release']['major'].to_i == 18
-                      '0.9.0'
-                    else
+      pcs_version = if facts[:os]['release']['major'].to_i > 18
                       '0.10.0'
+                    else
+                      '0.9.0'
                     end
     end
   when 'Suse'
--- spec/spec_helper_corosync.rb
+++ spec/spec_helper_corosync.rb
@@ -18,10 +18,10 @@ def corosync_stack(facts)
       pcs_version = '0.10.0'
     when 'Ubuntu'
       corosync_stack = 'pcs'
-      pcs_version = if facts[:os]['release']['major'].to_i == 18
-                      '0.9.0'
-                    else
+      pcs_version = if facts[:os]['release']['major'].to_i > 18
                       '0.10.0'
+                    else
+                      '0.9.0'
                     end
     end
   when 'Suse'
